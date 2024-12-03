251bdf4e65ba1510e727615f92223533c9ba530c
Sanskar Gupta <sg0184@srmist.edu.in>
2024-12-03 17:49:34 +0530

    Create PYTHON PROGRAM 2 - SQUARE ROOT.py

---
+++ PYTHON PROGRAM 2 - SQUARE ROOT.py
@@ -0,0 +1,11 @@
+#Importing library
+import math
+
+#Asking user for input
+num = int(input("Enter a number: "))
+
+#Calculating square root
+result = math.sqrt(num)
+
+#Printing result
+print(f"Square root of {num} is {result}")
